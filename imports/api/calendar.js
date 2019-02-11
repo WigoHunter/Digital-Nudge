2a9ba34e42a49d0e1a27ced0f5be05c13a8f0065
Kai-Chun Hsu <mattandkevin1060@gmail.com>
2019-02-10 21:52:32 -0500

    get event summary for next 24 hours

--- imports/api/calendar.js
+++ imports/api/calendar.js
@@ -1,9 +1,9 @@
 import { Meteor } from "meteor/meteor";
 
 // Params: calendars = list of calendar ids of each calendar the user has.
-const hasEventToday = calendars => {
+const hasEventToday = events => {
 	// TODO: Should check the calendars if there are events today.
-	return calendars;
+	return events.items.map(e => e.summary) || [];
 };
 
 Meteor.methods({
@@ -11,13 +11,17 @@ Meteor.methods({
 	"getCalendar"() {
 		// get calendar on the server side
 		return new Promise((resolve, reject) => {
-			GoogleApi.get("/calendar/v3/users/me/calendarList", { user: Meteor.user() }, (err, res) => {
+			GoogleApi.get("/calendar/v3/calendars/primary/events", {
+				params: {
+					timeMin: new Date().toISOString(),
+					timeMax: new Date(Date.now() + 1 * 24 * 60 * 60 * 1000).toISOString()
+				}
+			}, (err, res) => {
 				if (err) {
 					reject(err);
 				}
-	
-				const calendars = res;
-				resolve(hasEventToday(calendars));
+
+				resolve(hasEventToday(res));
 			});
 		});
 	},
