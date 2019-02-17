55c6860e28ccd5ec9310fc4e23a19180b75f694a
Kai-Chun Hsu <mattandkevin1060@gmail.com>
2019-02-17 14:59:14 -0500

    disabled eslint no-undef

--- server/main.js
+++ server/main.js
@@ -7,9 +7,7 @@ import "../imports/api/scheduledJob";
 import keys from "../keys";
 
 Meteor.startup(() => {
-	// eslint-disable-next-line no-undef
 	process.env.MAIL_URL = keys.sendGrid.server;
 	Meteor.http = HTTP;
-	// eslint-disable-next-line no-undef
 	SyncedCron.start();
 });
