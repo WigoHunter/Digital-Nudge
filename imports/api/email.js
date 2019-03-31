59a94d90fe9fdd5849153c630a387cd8a915065f
Bowen Zhang <bz335@cornell.edu>
2019-03-30 18:19:31 -0700

    migrate from smtp to api v3

--- imports/api/email.js
+++ imports/api/email.js
@@ -1,48 +1,28 @@
 import { Meteor } from "meteor/meteor";
 import { Email } from "meteor/email";
+import keys from "../../keys";
+import sgMail from "@sendgrid/mail";
 
+sgMail.setApiKey(keys["sendGrid"]["key"]);
 export const sendEmail = (events, user=Meteor.user()) => {
 	// Check if the user has gmail first. In case google.email runs into error.
 	if (user && user.services.google) {
 		console.log(`sending email to ${user.services.google.email}...`);
 
 		try {
-			Email.send({
-				to: user.services.google.email,
+			const msg = {
+				to: "bwzhangtopone@gmail.com",
 				from: "kevin@nudges.ml",
 				subject: "Hello From Digital Nudge",
-				text: "You are slacking off!",
-				html: "You are slacking off!",
-				headers: {
-				  "X-SMTPAPI": JSON.stringify({
-						"to": [
-					  user.services.google.email
-						],
-						"sub": {
-					  "[gcdate1]": [
-								"20190324T200000Z/20190324T210000Z"
-					  ],
-					  "[gcdate2]": [
-								"20190324T210000Z/20190324T220000Z"
-					  ],
-					  "-gcbtnname1-": [
-								"20:00 to 21:00"
-					  ],
-					  "-gcbtnname2-": [
-								"21:00 to 22:00"
-					  ],
-						},
-						"filters": {
-					  "templates": {
-								"settings": {
-									"enable": 1,
-									"template_id": "44a48ed5-95e8-4d63-adbf-69af89bacaa9"
-								}
-					  }
-						}
-				  })
-				}
-			});
+				templateId: "d-5934901c3a1d48048bdd247ef0166839",
+				dynamic_template_data: {
+					subject: "Hello From Digital Nudge",
+					gctext: "You are slacking off!",
+					gcdate: "20190324T220000Z/20190324T230000Z",
+					gcbtnname: "Work on project",
+				},
+			};
+			sgMail.send(msg);
 		} catch(e) {
 			console.log(e);
 		}
