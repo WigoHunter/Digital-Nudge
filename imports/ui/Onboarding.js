5cb5204e5901762bd78ed7c20fb49f2f90c315a7
Kai-Chun Hsu <mattandkevin1060@gmail.com>
2019-10-14 11:18:25 -0700

    fix the onSubmit issue

--- imports/ui/Onboarding.js
+++ imports/ui/Onboarding.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Meteor } from "meteor/meteor";
-import { flattenPreference, genTitle } from "../api/utils";
+import { genTitle } from "../api/utils";
 import { Config } from "../db/configs";
 import { withTracker } from "meteor/react-meteor-data";
+
+const _ = require("lodash");
 
 type Props = {
   setPreferences: () => void,
@@ -19,193 +21,179 @@ type Props = {
   }
 };
 
-class Onboarding extends React.Component<Props> {
-  constructor(props) {
-    super(props);
-
-    const preferences = flattenPreference(props.preferences) || {};
-
-    this.state = {
-      productivityClick: false,
-      wellnessClick: false,
-      leisureClick: false,
-      workOnSideProject: false,
-      workOnHomework: false,
-      reviewCourseMaterial: false,
-      workout: false,
-      playBasketball: false,
-      goJogging: false,
-      meditationToClearMind: false,
-      readToExpandMind: false,
-      readAFictionBook: false,
-      playVideoGames: false,
-      spendTimeWithLovedOnes: false,
-      hangOutAtABar: false,
-      haveAMealWithFriends: false,
-      playBoardGames: false,
-      goToAMovieWithFriends: false,
-      ...preferences
-    };
+const genPreferenceState = (preferenceConfig, preferences) => {
+  if (preferenceConfig == null || preferences == null) {
+    return {};
   }
 
-  handleSubmit = () => {
-    const preferences = {
-      productivity: {
-        self: {
-          workOnSideProject: this.state.workOnSideProject
-        },
-        work: {
-          workOnHomework: this.state.workOnHomework,
-          reviewCourseMaterial: this.state.reviewCourseMaterial
-        }
-      },
+  return Object.keys(preferenceConfig).reduce((result, category) => {
+    const subs = preferenceConfig[category];
+    const res = Object.keys(subs).reduce(
+      (primaryCategoryObject, subCategory) => {
+        const events = preferenceConfig[category][subCategory];
+        const thisSubCategoryObject = Object.keys(events).reduce(
+          (all, eventName) => {
+            return {
+              ...all,
+              [eventName]: _.get(
+                preferences,
+                `${category}.${subCategory}.${eventName}`,
+                false
+              )
+            };
+          },
+          {}
+        );
 
-      wellness: {
-        physical: {
-          workout: this.state.workout,
-          playBasketball: this.state.playBasketball,
-          goJogging: this.state.goJogging
-        },
-        mental: {
-          meditationToClearMind: this.state.meditationToClearMind,
-          readToExpandMind: this.state.readToExpandMind
-        }
+        return {
+          ...primaryCategoryObject,
+          [subCategory]: thisSubCategoryObject
+        };
       },
+      {}
+    );
 
-      leisure: {
-        personal: {
-          readAFictionBook: this.state.readAFictionBook,
-          playVideoGames: this.state.playVideoGames,
-          spendTimeWithLovedOnes: this.state.spendTimeWithLovedOnes
-        },
-        social: {
-          hangOutAtABar: this.state.hangOutAtABar,
-          haveAMealWithFriends: this.state.haveAMealWithFriends,
-          playBoardGames: this.state.playBoardGames,
-          goToAMovieWithFriends: this.state.goToAMovieWithFriends
-        }
-      }
+    return {
+      ...result,
+      [category]: res
     };
+  }, {});
+};
 
-    Meteor.call("updatePreferences", this.props.userId, preferences, () => {
+function Onboarding(props: Props) {
+  const { preferences, loading, config } = props;
+  const [state, setPreferenceState] = useState({});
+  const [opens, setOpens] = useState([false, false, false]);
+
+  useEffect(() => {
+    if (
+      !loading &&
+      config != null &&
+      config.eventPreferences != null &&
+      preferences != null
+    ) {
+      setPreferenceState(
+        genPreferenceState(config.eventPreferences, preferences)
+      );
+    }
+  }, [loading]);
+
+  const handleSubmit = () => {
+    Meteor.call("updatePreferences", props.userId, state, () => {
       setTimeout(() => {
-        this.props.setPreferences(false);
+        props.setPreferences(false);
       }, 300);
     });
   };
 
-  handleInputChange = event => {
+  const handleInputChange = (event, path) => {
     const target = event.target;
     const value = target.type === "checkbox" ? target.checked : target.value;
-    const name = target.name;
 
-    this.setState({
-      [name]: value
-    });
+    setPreferenceState(_.set({ ...state }, path, value));
   };
 
-  handleClick = event => {
-    const value = !this.state[event.target.id];
-    this.setState({
-      [event.target.id]: value
-    });
+  const handleClick = index => {
+    setOpens(_.set({ ...opens }, index, !opens[index]));
   };
 
-  render() {
-    let dropdowns = [null, null, null];
-    const { loading, config } = this.props;
+  let dropdowns = [null, null, null];
 
-    if (!loading && config.eventPreferences) {
-      Object.keys(config.eventPreferences).map((category, index) => {
-        const subCategory = config.eventPreferences[category];
+  if (!loading && config.eventPreferences) {
+    console.log(state);
 
-        if (subCategory) {
-          dropdowns[index] = (
-            <div className="categoryDropdown" key={"categoryDropdown" + index}>
-              {Object.keys(subCategory).map((title, idx) => {
-                const subTitles = Object.keys(subCategory[title]);
+    Object.keys(config.eventPreferences).map((category, index) => {
+      const subCategory = config.eventPreferences[category];
 
-                return (
-                  <div className="subCategory" key={"subCategory" + idx}>
-                    <b>{title}</b>
-                    <hr className="subCategorySplit"></hr>
-                    {subTitles.map(sub => (
+      if (subCategory) {
+        dropdowns[index] = (
+          <div className="categoryDropdown" key={"categoryDropdown" + index}>
+            {Object.keys(subCategory).map((title, idx) => {
+              const subTitles = Object.keys(subCategory[title]);
+
+              return (
+                <div className="subCategory" key={"subCategory" + idx}>
+                  <b>{title}</b>
+                  <hr className="subCategorySplit"></hr>
+                  {subTitles.map(sub => {
+                    const path = `${category}.${title}.${sub}`;
+                    console.log(path);
+
+                    return (
                       <label key={sub}>
                         <input
                           name={sub}
                           type="checkbox"
-                          checked={this.state[sub]}
-                          onClick={this.handleInputChange}
+                          checked={_.get(state, path, false)}
+                          onClick={e => handleInputChange(e, path)}
                         />{" "}
                         {genTitle(sub)}
                       </label>
-                    ))}
-                  </div>
-                );
-              })}
-            </div>
-          );
-        }
-      });
-    }
+                    );
+                  })}
+                </div>
+              );
+            })}
+          </div>
+        );
+      }
+    });
+  }
 
-    return (
-      <div className="categoryContainer content">
-        <h3>Welcome to Digital Nudge</h3>
-        <p className="sub" style={{ color: "#fff", margin: "5px 0 30px" }}>
-          Help us help you! Tell us what excites you and we will help you
-          schedule events you love!
-        </p>
-        <div className="categoryRow">
-          <div>
-            <div className="categoryCard" id="productivityCard">
-              <a
-                href="#"
-                id="productivityClick"
-                className="categoryClick"
-                onClick={this.handleClick}
-              >
-                Productivity
-              </a>
-            </div>
-            {this.state.productivityClick ? dropdowns[0] : null}
+  return (
+    <div className="categoryContainer content">
+      <h3>Welcome to Digital Nudge</h3>
+      <p className="sub" style={{ color: "#fff", margin: "5px 0 30px" }}>
+        Help us help you! Tell us what excites you and we will help you schedule
+        events you love!
+      </p>
+      <div className="categoryRow">
+        <div>
+          <div className="categoryCard" id="productivityCard">
+            <a
+              href="#"
+              id="productivityClick"
+              className="categoryClick"
+              onClick={() => handleClick(0)}
+            >
+              Productivity
+            </a>
           </div>
-          <div>
-            <div className="categoryCard" id="wellnessCard">
-              <a
-                href="#"
-                id="wellnessClick"
-                className="categoryClick"
-                onClick={this.handleClick}
-              >
-                Wellness
-              </a>
-            </div>
-            {this.state.wellnessClick ? dropdowns[1] : null}
-          </div>
-
-          <div>
-            <div className="categoryCard" id="leisureCard">
-              <a
-                href="#"
-                id="leisureClick"
-                className="categoryClick"
-                onClick={this.handleClick}
-              >
-                Leisure
-              </a>
-            </div>
-            {this.state.leisureClick ? dropdowns[2] : null}
-          </div>
+          {opens[0] ? dropdowns[0] : null}
         </div>
-        <div className="onboardingBtn" onClick={() => this.handleSubmit()}>
-          {Object.keys(this.props.preferences || {}).length > 0
-            ? "Close"
-            : "Confirm"}
+        <div>
+          <div className="categoryCard" id="wellnessCard">
+            <a
+              href="#"
+              id="wellnessClick"
+              className="categoryClick"
+              onClick={() => handleClick(1)}
+            >
+              Wellness
+            </a>
+          </div>
+          {opens[1] ? dropdowns[1] : null}
+        </div>
+
+        <div>
+          <div className="categoryCard" id="leisureCard">
+            <a
+              href="#"
+              id="leisureClick"
+              className="categoryClick"
+              onClick={() => handleClick(2)}
+            >
+              Leisure
+            </a>
+          </div>
+          {opens[2] ? dropdowns[2] : null}
         </div>
       </div>
-    );
-  }
+      <div className="onboardingBtn" onClick={() => handleSubmit()}>
+        {Object.keys(props.preferences || {}).length > 0 ? "Close" : "Confirm"}
+      </div>
+    </div>
+  );
 }
 
 export default withTracker(() => {
